13b060fba023ee890fd9fe1cc020aea3d90428fb
Generalfil <fille.anders@hotmail.com>
2018-06-26 16:21:02 +0200

    Added intial GUI

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour {
+
+    Camera camera;
+
+    private float startScaleX;
+    private float startScaleY;
+
+    // Use this for initialization
+    void Start () {
+
+        camera = Camera.main;
+        startScaleX = this.transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward, camera.transform.rotation * Vector3.up);
+    }
+}
